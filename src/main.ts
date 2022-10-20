042b48ecb9b54791607536aeaaa03755c298b6b2
Kenny <kennymarcos96@gmail.com>
2022-10-20 12:12:10 -0300

    Init

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Chama o carregador de elementos após a plataforma ter sido init
+defineCustomElements(window);
 
 if (environment.production) {
   enableProdMode();
